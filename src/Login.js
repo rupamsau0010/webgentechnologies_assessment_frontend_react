e7a92211de9038b5a487e4dc3c112e7e99e65e32
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-09-02 23:22:55 +0530

    React Authentication done. Login plus signup complete

---
+++ src/Login.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+export default class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  submit = (event) => {
+    event.preventDefault();
+
+    const payload = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    axios({
+      url: "/authentication/login",
+      method: "POST",
+      data: payload,
+    })
+      .then((res) => {
+        console.log("Success");
+        console.log(res);
+        if (res.data.status === "success") {
+          alert("Login Successful");
+          this.props.history.push("/");
+        } else {
+          if (res.data.errors.email.length) {
+            alert(res.data.errors.email);
+          } else if (res.data.errors.password.length) {
+            alert(res.data.errors.password);
+          }
+        }
+      })
+      .catch((err) => {
+        console.log("Internal server error");
+        alert("Internal Server Error. Please try again.");
+        console.log(err);
+      });
+  };
+
+  render() {
+    console.log("state: ", this.state);
+    return (
+      <div id="login">
+        <form onSubmit={this.submit}>
+          {/* <img  src={logo} alt="" className="img-fluid" /> */}
+          <p>Welcome to Webgen Technologies</p>
+          <input
+            className="form-control"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input
+            className="form-control"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+
+          <button class="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
